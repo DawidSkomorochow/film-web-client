e0a3189cc05fafc5d1e194afe44b67adb6e3071b
Dawid Skomorochow <dawid.skomorochow.work@gmail.com>
2023-12-09 13:58:00 +0100

    FILMWEB-01: Add fetching data from BE

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,20 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <HelloWorld :msg="msg" />
+    <div v-if="items.length">
+      <h2>Items from Backend:</h2>
+      <ul>
+        <li v-for="item in items" :key="item.id">{{ item.name }}</li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from "vue"; // Import necessary functions
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import { defineComponent, ref, onMounted } from "vue";
+import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
 
 export default defineComponent({
   components: {
@@ -15,9 +22,20 @@ export default defineComponent({
   },
   setup() {
     const msg = ref("Welcome to Your Vue.js + TypeScript App");
+    const items = ref([]); // Define a reactive property for the items
+
+    onMounted(async () => {
+      try {
+        const response = await axios.get("http://localhost:3000/movies"); // Replace with your backend URL
+        items.value = response.data;
+      } catch (error) {
+        console.error("There was an error fetching the items: ", error);
+      }
+    });
 
     return {
       msg,
+      items,
     };
   },
 });
