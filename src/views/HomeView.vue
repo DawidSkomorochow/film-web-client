f648e23f406081dbdbe839e12067c0432f9210c2
Dawid Skomorochow <dawid.skomorochow.work@gmail.com>
2023-12-09 20:55:04 +0100

    FILMWEB-01: Code refactor

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,16 +5,24 @@
     <div v-if="items.length">
       <h2>Items from Backend:</h2>
       <ul>
-        <li v-for="item in items" :key="item.id">{{ item.name }}</li>
+        <li v-for="(item, index) in items" :key="index">
+          {{ item.title }}
+        </li>
       </ul>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, onMounted } from "vue";
+import { defineComponent, ref, onMounted, toRaw } from "vue";
 import HelloWorld from "@/components/HelloWorld.vue";
 import axios from "axios";
+
+interface Item {
+  title: string;
+
+  // other properties if needed
+}
 
 export default defineComponent({
   components: {
@@ -22,16 +30,21 @@ export default defineComponent({
   },
   setup() {
     const msg = ref("Welcome to Your Vue.js + TypeScript App");
-    const items = ref([]); // Define a reactive property for the items
+    const items = ref<Item[]>([]); // Define a reactive property for the items
 
     onMounted(async () => {
       try {
-        const response = await axios.get("http://localhost:3000/movies"); // Replace with your backend URL
-        items.value = response.data;
+        const response = await axios.get("http://localhost:3000/movies");
+        items.value = response.data.results;
+
+        console.log("response: ", response);
+        console.log("Items:", toRaw(items.value)); // Logging the items after fetching
       } catch (error) {
         console.error("There was an error fetching the items: ", error);
       }
     });
+
+    // console.log("items: ", items);
 
     return {
       msg,
