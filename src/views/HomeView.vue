9e13e2ec7449b7dff0c21bc4e04edf17095e16c6
Dawid Skomorochow <dawid.skomorochow.work@gmail.com>
2023-12-09 13:00:05 +0100

    FILMWEB-01: Init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue"; // Import necessary functions
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+
+export default defineComponent({
+  components: {
+    HelloWorld,
+  },
+  setup() {
+    const msg = ref("Welcome to Your Vue.js + TypeScript App");
+
+    return {
+      msg,
+    };
+  },
+});
+</script>
